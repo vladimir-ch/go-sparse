81917ef6ae95f60672697a8e74974c2434d52972
Vladimir Chalupecky <vladimir.chalupecky@gmail.com>
2015-10-07 10:53:45 +0900

    Use Vector and mat64.Vector in MulMatVec and update tests

--- level2.go
+++ level2.go
@@ -4,18 +4,20 @@
 
 package sparse
 
+import "github.com/gonum/matrix/mat64"
+
 // MulMatVec multiplies the dense vector x by a sparse matrix A (or its
 // transpose) and adds the result to the dense vector y, i.e., it computes
 //
-//  y = alpha * op(A) * x + beta * y,
+//  y += alpha * op(A) * x,
 //
 // where op(A) is either A or Aᵀ.
-func MulMatVec(alpha float64, transA bool, a Matrix, x []float64, incx int, beta float64, y []float64, incy int) {
+func MulMatVec(y *mat64.Vector, alpha float64, transA bool, a Matrix, x *mat64.Vector) {
 	switch a := a.(type) {
 	case *CSR:
-		csrMulMatVec(alpha, transA, a, x, incx, beta, y, incy)
+		csrMulMatVec(y, alpha, transA, a, x)
 	case *DOK:
-		dokMulMatVec(alpha, transA, a, x, incx, beta, y, incy)
+		dokMulMatVec(y, alpha, transA, a, x)
 	default:
 		panic("unsupported matrix type")
 	}
--- level2_test.go
+++ level2_test.go
@@ -7,6 +7,8 @@ package sparse
 import (
 	"reflect"
 	"testing"
+
+	"github.com/gonum/matrix/mat64"
 )
 
 func TestMulMatVec(t *testing.T) {
@@ -16,8 +18,6 @@ func TestMulMatVec(t *testing.T) {
 		v    []float64
 
 		x     []float64
-		incx  int
-		incy  int
 		alpha float64
 		trans bool
 
@@ -31,8 +31,6 @@ func TestMulMatVec(t *testing.T) {
 			v: []float64{1, 2},
 
 			x:     []float64{0, 0},
-			incx:  1,
-			incy:  1,
 			alpha: 1,
 			trans: false,
 
@@ -46,8 +44,6 @@ func TestMulMatVec(t *testing.T) {
 			v: []float64{1, 2},
 
 			x:     []float64{1, 1},
-			incx:  1,
-			incy:  1,
 			alpha: 1,
 			trans: false,
 
@@ -61,8 +57,6 @@ func TestMulMatVec(t *testing.T) {
 			v: []float64{1, 2, -4, 3}, // 0 0  3
 
 			x:     []float64{1, 2, 3},
-			incx:  1,
-			incy:  1,
 			alpha: 1,
 			trans: false,
 
@@ -71,13 +65,11 @@ func TestMulMatVec(t *testing.T) {
 		{
 			r: 3,
 			c: 3,
-			i: []int{0, 1, 1, 2},      // 0 1  0
+			i: []int{0, 1, 1, 2},      // 0 1  0  ^T
 			j: []int{1, 0, 2, 2},      // 2 0 -4
 			v: []float64{1, 2, -4, 3}, // 0 0  3
 
 			x:     []float64{1, 2, 3},
-			incx:  1,
-			incy:  1,
 			alpha: 1,
 			trans: true,
 
@@ -86,37 +78,70 @@ func TestMulMatVec(t *testing.T) {
 		{
 			r: 3,
 			c: 3,
-			i: []int{0, 1, 1, 2},      // 0 1  0
+			i: []int{0, 1, 1, 2},      // 0 1  0  ^T
 			j: []int{1, 0, 2, 2},      // 2 0 -4
 			v: []float64{1, 2, -4, 3}, // 0 0  3
 
 			x:     []float64{1, 2, 3},
-			incx:  1,
-			incy:  1,
 			alpha: 2,
 			trans: true,
 
 			want: []float64{8, 2, 2},
 		},
+		{
+			r: 3,
+			c: 5,
+			i: []int{0, 0, 1, 1, 2, 2},       // 0 1  0 1  0
+			j: []int{1, 3, 0, 2, 2, 4},       // 2 0 -4 0  0
+			v: []float64{1, 1, 2, -4, 3, -5}, // 0 0  3 0 -5
+
+			x:     []float64{1, 2, 3, 4, 5},
+			alpha: 2,
+			trans: false,
+
+			want: []float64{12, -20, -32},
+		},
+		{
+			r: 3,
+			c: 5,
+			i: []int{0, 0, 1, 1, 2, 2},       // 0 1  0 1  0  ^T
+			j: []int{1, 3, 0, 2, 2, 4},       // 2 0 -4 0  0
+			v: []float64{1, 1, 2, -4, 3, -5}, // 0 0  3 0 -5
+
+			x:     []float64{1, 2, 3},
+			alpha: 2,
+			trans: true,
+
+			want: []float64{8, 2, 2, 2, -30},
+		},
 	} {
+		var r, c int
+		if test.trans {
+			r, c = test.c, test.r
+		} else {
+			r, c = test.r, test.c
+		}
+		x := mat64.NewVector(c, test.x)
+		y := mat64.NewVector(r, nil)
+
 		dok := NewDOK(test.r, test.c)
 		for i := 0; i < len(test.v); i++ {
 			dok.InsertEntry(test.i[i], test.j[i], test.v[i])
 		}
-		y := make([]float64, test.r*test.incy)
 
-		MulMatVec(test.alpha, test.trans, dok, test.x, test.incx, 1, y, test.incy)
-		if !reflect.DeepEqual(y, test.want) {
-			t.Errorf("test %d: unexpected result for DOK", id+1)
+		MulMatVec(y, test.alpha, test.trans, dok, x)
+		if !reflect.DeepEqual(y.RawVector().Data, test.want) {
+			t.Errorf("test %d: unexpected result for DOK, want = %v, got = %v", id+1, test.want, y.RawVector().Data)
 		}
 
-		for i := range y {
-			y[i] = 0
+		for i := 0; i < y.Len(); i++ {
+			y.SetVec(i, 0)
 		}
 		csr := NewCSR(dok)
-		MulMatVec(test.alpha, test.trans, csr, test.x, test.incx, 1, y, test.incy)
-		if !reflect.DeepEqual(y, test.want) {
-			t.Errorf("test %d: unexpected result for CSR", id+1)
+
+		MulMatVec(y, test.alpha, test.trans, csr, x)
+		if !reflect.DeepEqual(y.RawVector().Data, test.want) {
+			t.Errorf("test %d: unexpected result for CSR, want = %v, got = %v", id+1, test.want, y.RawVector().Data)
 		}
 	}
 }
